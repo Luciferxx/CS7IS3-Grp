d100a65d22d5dad259661e245eec6a159717a953
Bair Buyrchiyev <buyrchib@lg12-15.scss.tcd.ie>
2022-11-10 20:05:45 +0000

    add latimes to all index

--- src/main/java/tcd/CS7IS3/Utils/allDataIndexer.java
+++ src/main/java/tcd/CS7IS3/Utils/allDataIndexer.java
@@ -7,12 +7,14 @@ import org.apache.lucene.document.Document;
 import tcd.CS7IS3.data_indexers.fbisIndexer;
 import tcd.CS7IS3.data_indexers.fr94Indexer;
 import tcd.CS7IS3.data_indexers.ftIndexer;
+import tcd.CS7IS3.data_indexers.latimesIndexer;
 
 public class allDataIndexer {
 	static ArrayList<Document> allDocuments = new ArrayList<>();
 	static fbisIndexer fbisIn = new fbisIndexer();
 	static fr94Indexer fr94In = new fr94Indexer();
 	static ftIndexer ftIn = new ftIndexer();
+	static latimesIndexer latimesIn = new latimesIndexer();
 
 	// @Finn Please make the indexer class for the datasets that you are working on
 	// over here.
@@ -20,6 +22,7 @@ public class allDataIndexer {
 		allDocuments.addAll(fbisIn.getDocuments());
 		allDocuments.addAll(fr94In.getDocuments());
 		allDocuments.addAll(ftIn.getDocuments());
+		allDocuments.addAll(latimesIn.getDocuments());
 		// @Finn use addAll to add your lucene documents as per dataset to
 		// all_data_lucene_document.
 		return allDocuments;
